b316506519a81ac34b00ecf24b4c19359d72dc88
Stelios Kakoliris <stelioskakoliris@Stelioss-MacBook-Pro.local>
2023-03-15 18:44:16 +0000

    removed bg from seed pic

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -9,7 +9,7 @@ module.exports = [
     username: "grumpy19",
     name: "Paul Grump",
     avatar_url:
-      "db/data/development-data/Mr-Grumpy-3A.PNG-removebg-preview.png",
+      "https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013",
   },
   {
     username: "happyamy2016",
